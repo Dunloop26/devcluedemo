26ddb060ba20ba9a48d52c4e312932379072471a
dunas26 <jhonjamesillo@gmail.com>
2022-09-01 23:17:20 -0500

    UPDATE: SVG Repository path update

--- src/services/SvgRepository.ts
+++ src/services/SvgRepository.ts
@@ -3,7 +3,29 @@ const toDefault = (data: typeof import("*.svg")) => data.default.toString();
 type RepositoryPromise = { [key: string]: Promise<string> }
 const repositories: { [key: string]: RepositoryPromise } = {
 	developer: {
-		antonio: import("@assets/ui/PortraitAntonio.svg").then(toDefault),
+		antonio: import(`@assets/ui/PortraitAntonio.svg`).then(toDefault),
+		carlos: import(`@assets/ui/PortraitCarlos.svg`).then(toDefault),
+		carolina: import(`@assets/ui/PortraitCarolina.svg`).then(toDefault),
+		juan: import(`@assets/ui/PortraitJuan.svg`).then(toDefault),
+		juanita: import(`@assets/ui/PortraitJuanita.svg`).then(toDefault),
+		manuel: import(`@assets/ui/PortraitManuel.svg`).then(toDefault),
+		pedro: import(`@assets/ui/PortraitPedro.svg`).then(toDefault),
+	},
+	module: {
+		comprobanteContable: import('@assets/ui/ModuloComprobanteContable.svg').then(toDefault),
+		contabilidad: import('@assets/ui/ModuloContabilidad.svg').then(toDefault),
+		facturacion: import('@assets/ui/ModuloFacturacion.svg').then(toDefault),
+		nomina: import('@assets/ui/ModuloNomina.svg').then(toDefault),
+		recibo: import('@assets/ui/ModuloRecibo.svg').then(toDefault),
+		usuarios: import('@assets/ui/ModuloUsuarios.svg').then(toDefault),
+	},
+	error: {
+		404: import('@assets/ui/Error404.svg').then(toDefault),
+		encodingError: import('@assets/ui/ErrorEncodingError.svg').then(toDefault),
+		memoryOutOfRange: import('@assets/ui/ErrorMemoryOutOfRange.svg').then(toDefault),
+		nullPointer: import('@assets/ui/ErrorNullPointer.svg').then(toDefault),
+		stackOverflow: import('@assets/ui/ErrorStackOverFlow.svg').then(toDefault),
+		syntaxError: import('@assets/ui/ErrorSyntaxError.svg').then(toDefault),
 	}
 }
 
